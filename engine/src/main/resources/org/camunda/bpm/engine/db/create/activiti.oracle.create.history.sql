camunda/camunda-bpm-platform

chore(sql): create indexes time fields of historic incidents table
* to allow fast sorting of the table, e.g. when Optimize fetches
the data in pages.

related to #CAM-12383

engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.oracle.create.history.sql
@@ -396,6 +396,8 @@ create index ACT_IDX_HI_INCIDENT_PROC_KEY on ACT_HI_INCIDENT(PROC_DEF_KEY_);
 create index ACT_IDX_HI_INCIDENT_ROOT_PI on ACT_HI_INCIDENT(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_INCIDENT_PROCINST on ACT_HI_INCIDENT(PROC_INST_ID_);
 create index ACT_IDX_HI_INCIDENT_RM_TIME on ACT_HI_INCIDENT(REMOVAL_TIME_);
+create index ACT_IDX_HI_INCIDENT_CREATE_TIME on ACT_HI_INCIDENT(CREATE_TIME_);
+create index ACT_IDX_HI_INCIDENT_END_TIME on ACT_HI_INCIDENT(END_TIME_);
 
 create index ACT_IDX_HI_JOB_LOG_ROOT_PI on ACT_HI_JOB_LOG(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_JOB_LOG_PROCINST on ACT_HI_JOB_LOG(PROCESS_INSTANCE_ID_);

engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.oracle.drop.history.sql
@@ -83,6 +83,8 @@ drop index ACT_IDX_HI_INCIDENT_PROC_KEY;
 drop index ACT_IDX_HI_INCIDENT_ROOT_PI;
 drop index ACT_IDX_HI_INCIDENT_PROCINST;
 drop index ACT_IDX_HI_INCIDENT_RM_TIME;
+drop index ACT_IDX_HI_INCIDENT_CREATE_TIME;
+drop index ACT_IDX_HI_INCIDENT_END_TIME;
 
 drop index ACT_IDX_HI_JOB_LOG_ROOT_PI;
 drop index ACT_IDX_HI_JOB_LOG_PROCINST;

OrqueIO/OrqueIO

engine/src/main/resources/org/camunda/bpm/engine/db/create/activiti.oracle.create.history.sql
@@ -396,6 +396,8 @@ create index ACT_IDX_HI_INCIDENT_PROC_KEY on ACT_HI_INCIDENT(PROC_DEF_KEY_);
 create index ACT_IDX_HI_INCIDENT_ROOT_PI on ACT_HI_INCIDENT(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_INCIDENT_PROCINST on ACT_HI_INCIDENT(PROC_INST_ID_);
 create index ACT_IDX_HI_INCIDENT_RM_TIME on ACT_HI_INCIDENT(REMOVAL_TIME_);
+create index ACT_IDX_HI_INCIDENT_CREATE_TI on ACT_HI_INCIDENT(CREATE_TIME_);
+create index ACT_IDX_HI_INCIDENT_END_TIME on ACT_HI_INCIDENT(END_TIME_);
 
 create index ACT_IDX_HI_JOB_LOG_ROOT_PI on ACT_HI_JOB_LOG(ROOT_PROC_INST_ID_);
 create index ACT_IDX_HI_JOB_LOG_PROCINST on ACT_HI_JOB_LOG(PROCESS_INSTANCE_ID_);

engine/src/main/resources/org/camunda/bpm/engine/db/drop/activiti.oracle.drop.history.sql
@@ -83,6 +83,8 @@ drop index ACT_IDX_HI_INCIDENT_PROC_KEY;
 drop index ACT_IDX_HI_INCIDENT_ROOT_PI;
 drop index ACT_IDX_HI_INCIDENT_PROCINST;
 drop index ACT_IDX_HI_INCIDENT_RM_TIME;
+drop index ACT_IDX_HI_INCIDENT_CREATE_TI;
+drop index ACT_IDX_HI_INCIDENT_END_TIME;
 
 drop index ACT_IDX_HI_JOB_LOG_ROOT_PI;
 drop index ACT_IDX_HI_JOB_LOG_PROCINST;