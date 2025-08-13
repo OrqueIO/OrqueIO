65a44da0c036e6b07c35053d7c64a708fa6463fc
Zaghdoudi Mohamed <mohamed.zgh28@gmail.com>
2025-08-13 23:21:01 +0100

    task(freemarker): Camunda replaced with OrqueIO

--- freemarker-template-engine/src/test/java/io/orqueio/templateengines/engine/util/Greeter.java
+++ freemarker-template-engine/src/test/java/io/orqueio/templateengines/engine/util/Greeter.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.camunda.templateengines.engine.util;
+package io.orqueio.templateengines.engine.util;
 
 /**
  * @author Sebastian Menski
