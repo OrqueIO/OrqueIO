a88aa6928cf490416e32a277b280a9283c24c458
Shaymabenahmed-tech <shayma.benahmed@codeonce.fr>
2026-06-15 13:01:15 +0100

    chore: replace Camunda copyright header with OrqueIO copyright in new files

--- spring-boot-starter/starter-webapp-core/src/main/java/io/orqueio/bpm/spring/boot/starter/webapp/BaseHrefTransformer.java
+++ spring-boot-starter/starter-webapp-core/src/main/java/io/orqueio/bpm/spring/boot/starter/webapp/BaseHrefTransformer.java
@@ -1,8 +1,8 @@
 /*
- * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * Copyright OrqueIO and/or licensed to OrqueIO
  * under one or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information regarding copyright
- * ownership. Camunda licenses this file to you under the Apache License,
+ * ownership. OrqueIO licenses this file to you under the Apache License,
  * Version 2.0; you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.orqueio.bpm.spring.boot.starter.webapp;
 
 import jakarta.servlet.http.HttpServletRequest;
